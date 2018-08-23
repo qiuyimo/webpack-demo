f1e6292beeb9b57afe193bdbed7e5db2d64df21f
qiuyu <530004000@qq.com>
2018-08-23 13:28:17 +0800

    管理输出.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import _ from 'lodash'
-import './style.css'
-import Icon from './icon.png'
+import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
 
     return element;
 }
